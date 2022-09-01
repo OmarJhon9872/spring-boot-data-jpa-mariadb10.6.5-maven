2deed13ee9907942f88d62bc2e20d95b75b1367d
OmarJhon9872 <omarjonax0@gmail.com>
2022-08-31 23:35:07 -0500

    04 Implementacion de CRUD JPA y bind params

--- src/main/java/com/holamundothymeleaf/web/HolaMundo.java
+++ src/main/java/com/holamundothymeleaf/web/HolaMundo.java
@@ -1,11 +1,13 @@
 package com.holamundothymeleaf.web;
 
+import com.holamundothymeleaf.domain.Persona;
 import com.holamundothymeleaf.service.PersonaService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
@@ -23,5 +25,32 @@ public class HolaMundo {
 
         log.info("Ejecutando el metodo inicio");
         return "index";
+    }
+
+
+    @GetMapping("/agregar")
+    public String agregar(Persona persona){
+        return "modificar";
+    }
+
+    @PostMapping("/guardar")
+    public String guardar(Persona persona){
+        personaService.guardarPersona(persona);
+        return "redirect:/";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String editar(Persona persona, Model model){
+        persona = personaService.encontrarPersona(persona);
+
+        model.addAttribute("persona", persona);
+        return "modificar";
+    }
+
+    //@GetMapping("/eliminar/{id}")
+    @GetMapping("/eliminar")
+    public String eliminar(Persona persona, Model model){
+        personaService.eliminarPersona(persona);
+        return "redirect:/";
     }
 }
