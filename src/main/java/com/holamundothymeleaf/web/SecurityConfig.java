1dad83b9ebea2b2a802d0c4a6dce6d9527212414
gdc21 <developers.gdc.2021@gmail.com>
2022-09-02 18:11:09 -0500

    12 Implementacion de inicio de sesion con validacion de contraseñas encriptadas bcrypt y consultas en base de datos

--- src/main/java/com/holamundothymeleaf/web/SecurityConfig.java
+++ src/main/java/com/holamundothymeleaf/web/SecurityConfig.java
@@ -1,17 +1,20 @@
 package com.holamundothymeleaf.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
+    /*@Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
         auth.inMemoryAuthentication()
                 .withUser("admin")
@@ -21,8 +24,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser("user1")
                 .password("{noop}1234")
                 .roles("USER");
+    }*/
+    @Autowired
+    private UserDetailsService userDetailsService;
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
-
+//    @Autowired
+//    public void configurerGlobal(AuthenticationManagerBuilder build) throws Exception {
+    @Override
+    public void configure(AuthenticationManagerBuilder build) throws Exception {
+        build.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.authorizeRequests()
